062ba7c4d6687171ccf845cb977ca81f359e04a8
jgmchan <jgmchan@gmail.com>
2017-12-07 16:25:09 +1100

    Much better implementation with streams
    Thanks to https://github.com/hellomika/advent_of_code_2017/blob/master/lib/day_1.ex

--- test/day01_test.exs
+++ test/day01_test.exs
@@ -2,17 +2,17 @@ defmodule Advent.Day01Test do
   use ExUnit.Case
 
   test "Part 1" do
-    assert Advent.Day01.reverse_captcha('1122') == 3
-    assert Advent.Day01.reverse_captcha('1111') == 4
-    assert Advent.Day01.reverse_captcha('1234') == 0
-    assert Advent.Day01.reverse_captcha('91212129') == 9
+    assert Advent.Day01.solve_1('1122') == 3
+    assert Advent.Day01.solve_1('1111') == 4
+    assert Advent.Day01.solve_1('1234') == 0
+    assert Advent.Day01.solve_1('91212129') == 9
   end
 
   test "Part 2" do
-    assert Advent.Day01p2.reverse_captcha('1212') == 6
-    assert Advent.Day01p2.reverse_captcha('1221') == 0
-    assert Advent.Day01p2.reverse_captcha('123425') == 4
-    assert Advent.Day01p2.reverse_captcha('123123') == 12
-    assert Advent.Day01p2.reverse_captcha('12131415') == 4
+    assert Advent.Day01.solve_2('1212') == 6
+    assert Advent.Day01.solve_2('1221') == 0
+    assert Advent.Day01.solve_2('123425') == 4
+    assert Advent.Day01.solve_2('123123') == 12
+    assert Advent.Day01.solve_2('12131415') == 4
   end
 end
