7ce7ba3740b238ac4b9ec8fe52ecb29a24dffe41
jgmchan <jgmchan@gmail.com>
2017-12-09 08:11:21 +1100

    Day 03 - Needs some cleanup

---
+++ lib/day03.ex
@@ -0,0 +1,159 @@
+defmodule Advent.Day03.Board do
+  alias __MODULE__
+
+  defstruct active_coord: {0, 0},
+            state: %{}
+
+  def move(%Board{active_coord: active_coord} = board, :left),
+    do: move(board, position(:left_of, active_coord))
+
+  def move(%Board{active_coord: active_coord} = board, :right),
+    do: move(board, position(:right_of, active_coord))
+
+  def move(%Board{active_coord: active_coord} = board, :up),
+    do: move(board, position(:above_of, active_coord))
+
+  def move(%Board{active_coord: active_coord} = board, :down),
+    do: move(board, position(:below_of, active_coord))
+
+  def move(%Board{} = board, position) when is_tuple(position) do
+    %{board | active_coord: position}
+  end
+
+  def position(:left_of, {x, y}), do: {x - 1, y}
+  def position(:right_of, {x, y}), do: {x + 1, y}
+  def position(:above_of, {x, y}), do: {x, y + 1}
+  def position(:below_of, {x, y}), do: {x, y - 1}
+  def position(:top_right_of, {x, y}), do: {x + 1, y + 1}
+  def position(:top_left_of, {x, y}), do: {x - 1, y + 1}
+  def position(:bottom_left_of, {x, y}), do: {x - 1, y - 1}
+  def position(:bottom_right_of, {x, y}), do: {x + 1, y - 1}
+
+  def active_value(%Board{active_coord: active_coord} = board) do
+    get_value(board, active_coord)
+  end
+
+  def get_value(%Board{state: state}, coord) do
+    Map.get(state, coord)
+  end
+
+  def set_value(%Board{state: state} = board, coord, value) do
+    %{board | state: Map.put(state, coord, value)}
+  end
+
+  def surrounding_values(%Board{} = board, coord) do
+    value = fn of -> get_value(board, position(of, coord)) end
+
+    %{
+      top_left: value.(:top_left_of),
+      above: value.(:above_of),
+      top_right: value.(:top_right_of),
+      left: value.(:left_of),
+      right: value.(:right_of),
+      below: value.(:below_of),
+      bottom_left: value.(:bottom_left_of),
+      bottom_right: value.(:bottom_right_of)
+    }
+  end
+
+  def next_spiral_move(%{left: v, above: nil}) when not is_nil(v), do: :up
+  def next_spiral_move(%{below: v, left: nil}) when not is_nil(v), do: :left
+  def next_spiral_move(%{right: v, below: nil}) when not is_nil(v), do: :down
+  def next_spiral_move(%{above: v, right: nil}) when not is_nil(v), do: :right
+  def next_spiral_move(%{right: nil, left: nil, above: nil, below: nil}), do: :right
+end
+
+defmodule Advent.Day03 do
+  alias Advent.Day03.Board
+  @input 347_991
+
+  # Part 1
+  # 17  16  15  14  13
+  # 18   5   4   3  12
+  # 19   6   1   2  11
+  # 20   7   8   9  10
+  # 21  22  23  24  25
+  #
+
+  # Part 2
+  # 147  142  133  122   59
+  # 304    5    4    2   57
+  # 330   10    1    1   54
+  # 351   11   23   25   26
+  # 362  747  806
+  #
+
+  def solve_1, do: steps(@input)
+
+  def solve_2, do: part_2(@input)
+
+  def part_2(target, num \\ 1, value \\ 0)
+  def part_2(target, num, value) when value > target, do: value
+  def part_2(target, num, _value), do: part_2(target, num + 1, moves(num))
+
+  def moves(num) do
+    next_step = fn board ->
+      next_move =
+        board
+        |> Board.surrounding_values(board.active_coord)
+        |> Board.next_spiral_move()
+
+      board = Board.move(board, next_move)
+
+      Board.set_value(
+        board,
+        board.active_coord,
+        Map.values(Board.surrounding_values(board, board.active_coord)) |> Enum.filter(& &1)
+        |> Enum.sum()
+      )
+    end
+
+    board = %Board{state: %{{0, 0} => 1}}
+
+    Enum.reduce(2..num, board, fn _, acc ->
+      acc |> next_step.()
+    end)
+    |> Board.active_value()
+  end
+
+  def steps(num) do
+    level = find_level(num)
+    ranges = side_ranges(level)
+    target_range = Enum.find(ranges, fn range -> num in range end)
+    mid = midpoint(target_range)
+    distance = abs(mid - num)
+    real_level = div(level - 1, 2)
+    distance + real_level
+  end
+
+  #    div(level - 1, 2) + 1)
+  def find_level(num, level \\ 1)
+  def find_level(1, _), do: 1
+  def find_level(num, level) when level * level > num, do: level
+  def find_level(num, level), do: find_level(num, level + 2)
+
+  defp side_ranges(level) do
+    prev_level = level - 2
+    min = prev_level * prev_level + 1
+    max = level * level
+    size = max - min + 1
+    side_length = div(size, 4)
+
+    for side <- 0..3 do
+      range_min = min + side * side_length
+      range_max = range_min + side_length - 1
+
+      range_min..range_max
+    end
+  end
+
+  def midpoint(range) do
+    len = range.last - range.first + 1
+    half_len = div(len, 2)
+    range.first + half_len - 1
+  end
+
+  def part_1(num) do
+    steps(num)
+  end
+end
---
+++ test/day03_test.exs
@@ -0,0 +1,98 @@
+defmodule Advent.Day03Test do
+  use ExUnit.Case
+
+  describe "Board" do
+    alias Advent.Day03.Board
+
+    test "movement" do
+      board = %Board{}
+
+      assert Board.move(board, :left).active_coord == {-1, 0}
+      assert Board.move(board, :right).active_coord == {1, 0}
+      assert Board.move(board, :up).active_coord == {0, 1}
+      assert Board.move(board, :down).active_coord == {0, -1}
+      assert Board.move(board, {2, 3}).active_coord == {2, 3}
+    end
+
+    test "values" do
+      board =
+        %Board{}
+        |> Board.set_value({0, 0}, 100)
+
+      assert Board.get_value(board, {0, 0}) == 100
+      assert Board.get_value(board, {1, 1}) == nil
+      assert Board.active_value(board) == 100
+    end
+
+    test "surrounding_values" do
+      state = %{
+        {0, 0} => 1,
+        {1, 0} => 2,
+        {1, 1} => 3,
+        {0, 1} => 4,
+        {-1, 1} => 5,
+        {-1, 0} => 6,
+        {-1, -1} => 7,
+        {0, -1} => 8,
+        {1, -1} => 9
+      }
+
+      board = %Board{state: state}
+
+      assert Board.surrounding_values(board, {0, 0}) == %{
+               top_left: 5,
+               above: 4,
+               top_right: 3,
+               left: 6,
+               right: 2,
+               below: 8,
+               bottom_left: 7,
+               bottom_right: 9
+             }
+
+      assert Board.surrounding_values(board, {1, 0}) == %{
+               top_left: 4,
+               above: 3,
+               top_right: nil,
+               left: 1,
+               right: nil,
+               bottom_left: 8,
+               below: 9,
+               bottom_right: nil
+             }
+    end
+  end
+
+  @tag :skip
+  test "find_level" do
+    assert Advent.Day03.find_level(1) == 1
+    assert Advent.Day03.find_level(2) == 3
+    assert Advent.Day03.find_level(14) == 5
+    assert Advent.Day03.find_level(26) == 7
+    assert Advent.Day03.find_level(79) == 9
+  end
+
+  @tag :skip
+  test "midpoint" do
+    assert Advent.Day03.midpoint(10..13) == 11
+    assert Advent.Day03.midpoint(14..17) == 15
+    assert Advent.Day03.midpoint(18..21) == 19
+    assert Advent.Day03.midpoint(22..25) == 23
+  end
+
+  @tag :skip
+  test "Part 1" do
+    assert Advent.Day03.part_1(1) == 0
+    assert Advent.Day03.part_1(12) == 3
+    assert Advent.Day03.part_1(23) == 2
+    assert Advent.Day03.part_1(1024) == 31
+  end
+
+  test "Part 2" do
+    #    assert Advent.Day03.part_2(1) == 1
+    assert Advent.Day03.moves(2) == 1
+    assert Advent.Day03.moves(3) == 2
+    assert Advent.Day03.moves(4) == 4
+    assert Advent.Day03.moves(5) == 5
+  end
+end
